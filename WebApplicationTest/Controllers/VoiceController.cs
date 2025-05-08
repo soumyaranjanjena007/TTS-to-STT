44b8b03b4efeeefcfa5dd2daa1922aae04615fa4
Soumya Ranjan Jena <soumya.omitri@gmail.com>
2025-05-08 12:24:54 +0530

    Add project files.

---
+++ WebApplicationTest/Controllers/VoiceController.cs
@@ -0,0 +1,92 @@
+﻿using CoreEntities.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+//using WebApplicationTest.Models;
+using WebApplicationTest.ServiceLayer;
+
+namespace WebApplicationTest.Controllers
+{
+    public class VoiceController : Controller
+    {
+        private readonly IServiceVoice _voiceService;
+        private static byte[] _audioBytes;
+        public VoiceController()
+        {
+            _voiceService = new VoiceSerivce();
+        }
+        // GET: Voice
+        public ActionResult Index()
+        {
+            
+            return View();
+        }
+
+        public ActionResult TTS()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> TTS(VoiceVM model)
+        {
+            if (model != null)
+            {
+                _audioBytes = await _voiceService.GetSpeechAsync(model.text ?? "Hello World, wellcome to gateway ai systems, how can i help you, i am soumya here to assist you");
+                TempData["Audio"] = _audioBytes;
+                ViewBag.AudioAvailable = true;
+            }         
+        
+
+            return View();
+        }
+
+        public async Task<ActionResult> STT(){
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> STT(VoiceVM voice)
+        {
+            var file = Request.Files["audioFile"];
+            if (file == null || file.ContentLength == 0)
+                return Content("No audio uploaded");
+
+            using (var client = new HttpClient())
+            {
+                var subscriptionKey = "";
+                var region = "eastus"; // or your region
+                var endpoint = $"https://{region}.stt.speech.microsoft.com/speech/recognition/conversation/cognitiveservices/v1?language=en-US";
+
+                client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", subscriptionKey);
+                client.DefaultRequestHeaders.Add("Accept", "application/json");
+
+                using (var content = new StreamContent(file.InputStream))
+                {
+                    content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("audio/wav");
+
+                    var response = await client.PostAsync(endpoint, content);
+                    var json = await response.Content.ReadAsStringAsync();
+
+                    dynamic result = Newtonsoft.Json.JsonConvert.DeserializeObject(json);
+                    return Content((string)result.DisplayText ?? "Unable to transcribe");
+                }
+            }
+        }
+
+
+        public ActionResult PlayAudio()
+        {
+            
+            if (_audioBytes == null)
+                return HttpNotFound();
+
+            return File(_audioBytes, "audio/mpeg");
+        }
+    }
+}
---
+++ WebApplicationTest/ServiceLayer/VoiceSerivce.cs
@@ -0,0 +1,60 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace WebApplicationTest.ServiceLayer
+{
+    public class VoiceSerivce : IServiceVoice
+    {
+        private readonly string apiKey = "EJkZMEyoDmUqPhdfdD8e9tGkJQildCtCmKSVBdfVdB0RmXdRyp30JQQJ99BEACYeBjFXJ3w3AAAYACOGMBd1";
+        private readonly string region = "eastus";
+        private readonly string endpoint = "https://eastus.tts.speech.microsoft.com/cognitiveservices/v1";
+
+        public async Task<byte[]> GetSpeechAsync(string text)
+        {
+            var accTok = await FetchTokenAsync();
+
+            using(var client =  new HttpClient())
+            {
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accTok);
+                client.DefaultRequestHeaders.Add("User-Agent", "Test GatewayAISystems");
+                client.DefaultRequestHeaders.Add("X-Microsoft-OutputFormat", "audio-16khz-32kbitrate-mono-mp3");
+
+
+                var body = $@"
+                        <speak version='1.0' xml:lang='en-US'>
+                        <voice xml:lang='en-US' xml:gender='Female' name='en-US-JennyNeural'>
+                        {System.Net.WebUtility.HtmlEncode(text)}
+                        </voice>
+                        </speak>";
+
+                using (var content = new StringContent(body, Encoding.UTF8, "application/ssml+xml"))
+                using (var response = await client.PostAsync(endpoint, content))
+                {
+                    response.EnsureSuccessStatusCode();
+                    return await response.Content.ReadAsByteArrayAsync();
+                }
+            }
+        }
+
+        private async Task<string> FetchTokenAsync()
+        {
+            using(var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", apiKey);
+                var uri = $"https://{region}.api.cognitive.microsoft.com/sts/v1.0/issueToken";
+
+                var response = await client.PostAsync(uri, null);
+                response.EnsureSuccessStatusCode();
+                return await response.Content.ReadAsStringAsync();
+            }
+        }
+    }
+}
